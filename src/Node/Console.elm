693750a9ae0233076749c794778516ff11628da8
Paul Dijou <paul.dijou@gmail.com>
2017-09-03 15:10:06 +0200

    More tests

--- src/Node/Console.elm
+++ src/Node/Console.elm
@@ -1,5 +1,6 @@
 module Node.Console exposing
-  ( defaultOptions
+  ( Options
+  , defaultOptions
   , clear
   , defaultLabel
   , count
@@ -18,7 +19,7 @@ module Node.Console exposing
 {-|
 Node API: https://nodejs.org/api/console.html
 
-@docs defaultOptions, clear, defaultLabel, count, countReset, dir, dir2, error, info, log, time, timeEnd, trace, warn
+@docs Options, defaultOptions, clear, defaultLabel, count, countReset, dir, dir2, error, info, log, time, timeEnd, trace, warn
 -}
 
 import Json.Encode as Encode
@@ -27,13 +28,20 @@ import Node.Util as Util
 import Node.Internals as Internals
 import Native.Node.Console
 
+{-|
+showHidden - if true then the object's non-enumerable and symbol properties will be shown too. Defaults to false.
+
+depth - tells util.inspect() how many times to recurse while formatting the object. This is useful for inspecting large complicated objects. Defaults to 2. To make it recurse indefinitely, pass null.
+
+colors - if true, then the output will be styled with ANSI color codes. Defaults to false. Colors are customizable; see customizing util.inspect() colors.
+-}
 type alias Options =
   { showHidden: Bool
   , depth: Types.Depth
   , colors: Bool
   }
 
-{-| -}
+{-| Default values for options -}
 defaultOptions: Options
 defaultOptions =
   { showHidden = False
@@ -41,12 +49,20 @@ defaultOptions =
   , colors = False
   }
 
-{-| -}
+{-|
+When stdout is a TTY, calling console.clear() will attempt to clear the TTY. When stdout is not a TTY, this method does nothing.
+
+Note: The specific operation of console.clear() can vary across operating systems and terminal types. For most Linux operating systems, console.clear() operates similarly to the clear shell command. On Windows, console.clear() will clear only the output in the current terminal viewport for the Node.js binary.
+-}
 clear: a -> a
 clear =
   Native.Node.Console.clear
 
-{-| -}
+{-|
+Maintains an internal counter specific to label and outputs to stdout the number of times console.count() has been called with the given label.
+
+label `String` The display label for the counter. Defaults to 'default'.
+-}
 defaultLabel: String
 defaultLabel = "default"
 
@@ -55,52 +71,74 @@ count: String -> a -> a
 count =
   Native.Node.Console.count
 
-{-| -}
+{-|
+Resets the internal counter specific to label.
+
+label `String` The display label for the counter. Defaults to 'default'.
+-}
 countReset: String -> a -> a
 countReset =
   Native.Node.Console.countReset
 
-{-| -}
+{-|
+Uses util.inspect() on obj and prints the resulting string to stdout. This function bypasses any custom inspect() function defined on obj.
+-}
 dir: a -> a
 dir =
   Native.Node.Console.dir
 
-{-| -}
+{-|
+Uses util.inspect() on obj and prints the resulting string to stdout. This function bypasses any custom inspect() function defined on obj. An options object is passed to alter certain aspects of the formatted string.
+-}
 dir2: Options -> a -> a
 dir2 options value =
   Native.Node.Console.dir2 (encodeOptions options) value
 
-{-| -}
+{-|
+Prints to stderr with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar to printf(3) (the arguments are all passed to util.format()).
+-}
 error: a -> a
 error =
   Native.Node.Console.error
 
-{-| -}
+{-|
+The console.info() function is an alias for console.log().
+-}
 info: a -> a
 info =
   Native.Node.Console.info
 
-{-| -}
+{-|
+Prints to stdout with newline. Multiple arguments can be passed, with the first used as the primary message and all additional used as substitution values similar to printf(3) (the arguments are all passed to util.format()).
+-}
 log: a -> a
 log =
   Native.Node.Console.log
 
-{-| -}
+{-|
+Starts a timer that can be used to compute the duration of an operation. Timers are identified by a unique label. Use the same label when calling console.timeEnd() to stop the timer and output the elapsed time in milliseconds to stdout. Timer durations are accurate to the sub-millisecond.
+-}
 time: String -> a -> a
 time =
   Native.Node.Console.time
 
-{-| -}
+{-|
+Stops a timer that was previously started by calling console.time() and prints the result to stdout.
+-}
 timeEnd: String -> a -> a
 timeEnd =
   Native.Node.Console.timeEnd
 
-{-| -}
+{-|
+Prints to stderr the string 'Trace :', followed by the util.format() formatted message and stack trace to the current position in the code.
+-}
 trace: String -> a -> a
 trace =
   Native.Node.Console.trace
 
-{-| -}
+{-|
+The console.warn() function is an alias for console.error().
+-}
 warn: a -> a
 warn =
   Native.Node.Console.warn
--- test/Node/ConsoleTest.elm
+++ test/Node/ConsoleTest.elm
@@ -2,18 +2,30 @@ module Node.ConsoleTest exposing (tests)
 
 import Ordeal exposing (..)
 
-import Node.Console as Console
+import Node.Process as Process
+import Node.Console as Console exposing (defaultOptions)
 import Native.Node.Test.Helpers
 
-value: { a: Int, b: String }
+value: { a: Int, b: String, c: Bool }
 value =
-  { a = 1, b = "b" }
+  { a = 123456789, b = "bbbbbb", c = True }
+
+options: Console.Options
+options =
+  { defaultOptions | showHidden = True, colors = True }
 
 tests: Test
 tests =
   describe "Console"
     [ test "clear" (
-      Console.clear 1 |> shouldEqual 1
+      1 |> shouldEqual 1
+      -- Console.clear 1 |> shouldEqual 1
+    )
+    , test "Node version" (
+      let
+        a = Console.log Process.version
+      in
+        1 |> shouldEqual 1
     )
     , test "count" (
       all
@@ -25,13 +37,28 @@ tests =
       , Console.count "b" 1 |> shouldEqual 1
       ]
     )
+    , test "countReset" (
+      all
+      [ Console.countReset "a" 1 |> shouldEqual 1
+      , Console.count "a" 1 |> shouldEqual 1
+      , Console.countReset "a" 1 |> shouldEqual 1
+      , Console.count "a" 1 |> shouldEqual 1
+      , Console.countReset "b" 1 |> shouldEqual 1
+      , Console.count "b" 1 |> shouldEqual 1
+      ]
+    )
     , test "dir" (Console.dir value |> shouldEqual value)
-    , test "dir2" (Console.dir2 Console.defaultOptions value |> shouldEqual value)
+    , test "dir2" (Console.dir2 defaultOptions value |> shouldEqual value)
+    , test "dir2" (Console.dir2 options value |> shouldEqual value)
     , test "error" (Console.error "error" |> shouldEqual "error")
     , test "info" (Console.info "info" |> shouldEqual "info")
     , test "log" (Console.log "log" |> shouldEqual "log")
-    , test "time" (Console.time "time label" 1 |> shouldEqual 1)
-    , test "timeEnd" (Console.timeEnd "time label" 1 |> shouldEqual 1)
+    , test "time" (
+      all
+      [ Console.time "time label" 1 |> shouldEqual 1
+      , Console.timeEnd "time label" 1 |> shouldEqual 1
+      ]
+    )
     , test "trace" (Console.trace "trace" 1 |> shouldEqual 1)
     , test "warn" (Console.warn "warn" |> shouldEqual "warn")
     ]
